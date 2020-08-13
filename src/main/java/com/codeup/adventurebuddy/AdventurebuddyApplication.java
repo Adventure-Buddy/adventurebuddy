7b7830d57ec4e4120d6f42a295568ea1e3a8c4ed
Larry Castillo <larrycastillo2009@gmail.com>
2020-08-13 10:13:30 -0500

    first commit

---
+++ src/main/java/com/codeup/adventurebuddy/AdventurebuddyApplication.java
@@ -0,0 +1,13 @@
+package com.codeup.adventurebuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AdventurebuddyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AdventurebuddyApplication.class, args);
+    }
+
+}
