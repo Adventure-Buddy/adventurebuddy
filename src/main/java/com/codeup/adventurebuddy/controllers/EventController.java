db9a537620f50c37d72d5bdb3f114b31ee9d8a5c
trevormullis12 <trevor.mullis12@gmail.com>
2020-08-20 13:39:13 -0500

    edit function

--- src/main/java/com/codeup/adventurebuddy/controllers/EventController.java
+++ src/main/java/com/codeup/adventurebuddy/controllers/EventController.java
@@ -55,4 +55,25 @@ public class EventController {
         eventDao.save(event);
         return "redirect:/events";
     }
+
+    @GetMapping("events/{id}/edit")
+    public String getEditEvent(@PathVariable long id, Model model) {
+        model.addAttribute("event", eventDao.getOne(id));
+        return "/events/edit";
+    }
+
+    @PostMapping("events/{id}/edit")
+    public String postEditEvent(@PathVariable long id,
+                                @RequestParam String title,
+                                @RequestParam String description,
+                                @RequestParam String date) {
+        Event eventToEdit = eventDao.getOne(id);
+        eventToEdit.setTitle(title);
+        eventToEdit.setDescription(description);
+        eventToEdit.setDate(date);
+
+        eventDao.save(eventToEdit);
+
+        return "redirect:/events/" + id;
+    }
 }
