b11491bebe08645cd020a0502d6ef0ea57530e98
trevormullis12 <trevor.mullis12@gmail.com>
2020-08-20 10:20:50 -0500

    finished adding and viewing individual events

---
+++ src/main/java/com/codeup/adventurebuddy/Repositories/TrailRepository.java
@@ -0,0 +1,7 @@
+package com.codeup.adventurebuddy.repositories;
+
+import com.codeup.adventurebuddy.models.Trail;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface TrailRepository extends JpaRepository<Trail, Long> {
+}
