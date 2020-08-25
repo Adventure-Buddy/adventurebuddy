bd4ed98257e273a9610dbada07c6fa0b142ea562
Fernando Hernandez <Fernando.Hernandez.Fabars@gmail.com>
2020-08-25 14:21:46 -0500

    dud

--- src/main/java/com/codeup/adventurebuddy/repositories/UserEventRepository.java
+++
@@ -1,9 +0,0 @@
-//package com.codeup.adventurebuddy.repositories;
-//
-//import com.codeup.adventurebuddy.models.UserEvents;
-//import org.springframework.data.jpa.repository.JpaRepository;
-//
-//public interface UserEventRepository extends JpaRepository<UserEvents, Long> {
-//
-//
-//}
